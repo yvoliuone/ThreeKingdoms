f366551482ac1a6ea24a8a492229ffb315759ad4
yvoliuone <yvoliuone@gmail.com>
2017-12-22 12:00:22 +0800

    init

--- src/main/java/server/ThreeKingdomsServer.java
+++ src/main/java/server/ThreeKingdomsServer.java
@@ -2,12 +2,14 @@ package server;
 
 import org.apache.log4j.BasicConfigurator;
 import static spark.Spark.*;
+import static j2html.TagCreator.html;
 
 public class ThreeKingdomsServer {
 
   public static void main(String[] args) {
     BasicConfigurator.configure();
     get("/hello", (req, res) -> "石肥肥胖胖是个大傻子");
+
   }
 
 }
