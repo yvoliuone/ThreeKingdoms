cc9dc1f0e093d8ed6ec48ace8d9a2adcc6535c53
yvoliuone <yvoliuone@gmail.com>
2017-12-22 12:03:17 +0800

    init

--- src/main/java/server/ThreeKingdomsServer.java
+++ src/main/java/server/ThreeKingdomsServer.java
@@ -8,8 +8,13 @@ public class ThreeKingdomsServer {
 
   public static void main(String[] args) {
     BasicConfigurator.configure();
-    get("/hello", (req, res) -> "石肥肥胖胖是个大傻子");
+    get("/hello", (req, res) -> "石肥肥胖胖是个大傻子"); // test
 
+
+    staticFileLocation("/public"); //index.html is served at localhost:4567 (default port)
+    webSocket("/chat", handler.class);
+    init();
+    
   }
 
 }
---
+++ src/main/java/server/handler.java
@@ -0,0 +1,7 @@
+package server;
+
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+
+@WebSocket
+public class handler {
+}
