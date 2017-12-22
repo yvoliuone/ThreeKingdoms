5891745a52d629357b7813dc7c766762755be717
Ashsandra <ts40@rice.edu>
2017-12-22 11:37:07 +0800

    changed server slightly

--- src/main/java/server/ThreeKingdomsServer.java
+++ src/main/java/server/ThreeKingdomsServer.java
@@ -8,7 +8,7 @@ public class ThreeKingdomsServer {
 
   public static void main(String[] args) {
     BasicConfigurator.configure();
-    get("/hello", (req, res) -> "hello world");
+    get("/hiYvonne", (req, res) -> "Yvonne is stupid");
   }
 
 }
