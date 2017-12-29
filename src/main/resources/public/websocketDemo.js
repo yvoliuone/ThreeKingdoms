7c09b4ef873dbeb8682d4d449e19770719b7def8
yvoliuone <yvoliuone@gmail.com>
2017-12-29 23:06:00 +0800

    WebSocket Learning Notes

--- src/main/resources/public/websocketDemo.js
+++ src/main/resources/public/websocketDemo.js
@@ -1,50 +1,85 @@
 //Establish the WebSocket connection and set up event handlers
 var webSocket = new WebSocket("ws://" + location.hostname + ":" + location.port + "/chat");
-webSocket.onmessage = function (msg) { updateChat(msg); };
+webSocket.onmessage = function (msg) { update(msg); };
 //webSocket.onclose = function () { alert("WebSocket connection closed") };
 
-//Send message if "Send" is clicked
-id("card1").addEventListener("click", function () {
-    sendMessage(JSON.stringify({play: 1}));
+// $("#card1").click( function () {
+//     alert("card1 is played!");
+//     console.log("clicked card1");
+//     sendJson("1");
+// });
+document.getElementById("card1").addEventListener("click", function () {
+    alert("card1 is played!");
+    sendJson("1");
+})
+
+$("#card2").click( function () {
+    alert("card2 is played!")
+    sendJson("2");
 });
 
-id("card2").addEventListener("click", function () {
-    sendMessage(JSON.stringify({play: 2}));
+$("#card3").click(function () {
+    sendJson("3");
 });
 
-id("card3").addEventListener("click", function () {
-    sendMessage(JSON.stringify({play: 3}));
+$("#card11").click(function () {
+    sendJson("11");
 });
 
-//Send message if enter is pressed in the input field
-id("message").addEventListener("keypress", function (e) {
-    if (e.keyCode === 13) { sendMessage(e.target.value); }
+$("#card22").click(function () {
+    sendJson("22");
 });
+
+$("#card33").click(function () {
+    sendJson("33");
+})
+
+// //Send message if enter is pressed in the input field
+// id("message").addEventListener("keypress", function (e) {
+//     if (e.keyCode === 13) { sendMessage(e.target.value); }
+// });
 
 //Send a message if it's not empty, then clear the input field
-function sendMessage(message) {
-    if (message !== "") {
-        webSocket.send(message);
-        // id("message").value = "";
-    }
+
+function sendJson(string) {
+    webSocket.send(JSON.stringify({play: string}));
+    var card = "#card" + string;
+    $(card).remove();
 }
 
-//Update the chat-panel, and the list of connected users
-function updateChat(msg) {
-    var data = JSON.parse(msg.data);
-    insert("chat", data.userMessage);
-    id("userlist").innerHTML = "";
-    data.userlist.forEach(function (user) {
-        insert("userlist", "<li>" + user + "</li>");
-    });
+function update(message) {
+    $.ajax({
+        url: "/testget/",
+        type: "GET",
+        data: {'input': message},
+    })
+        .done( function(data) {
+            alert("success: " + data);
+            var card = "card" + JSON.parse(data).play;
+            $(card).remove();
+    })
+        .fail( function(data) {
+            console.log("error: " + data)
+        })
 }
 
-//Helper function for inserting HTML as the first child of an element
-function insert(targetId, message) {
-    id(targetId).insertAdjacentHTML("afterbegin", message);
-}
 
-//Helper function for selecting element by id
-function id(id) {
-    return document.getElementById(id);
-}
+// //Update the chat-panel, and the list of connected users
+// function updateChat(msg) {
+//     var data = JSON.parse(msg.data);
+//     insert("chat", data.userMessage);
+//     id("userlist").innerHTML = "";
+//     data.userlist.forEach(function (user) {
+//         insert("userlist", "<li>" + user + "</li>");
+//     });
+// }
+
+// //Helper function for inserting HTML as the first child of an element
+// function insert(targetId, message) {
+//     id(targetId).insertAdjacentHTML("afterbegin", message);
+// }
+//
+// //Helper function for selecting element by id
+// function id(id) {
+//     return document.getElementById(id);
+// }
