58f5331934df100ca5f767e9dfa4513ded42152d
yvoliuone <yvoliuone@gmail.com>
2018-01-02 14:20:42 +0800

    WebSocket Learning Notes (实现手牌同步更新）

--- src/main/resources/public/websocketDemo.js
+++ src/main/resources/public/websocketDemo.js
@@ -1,43 +1,48 @@
-//Establish the WebSocket connection and set up event handlers
-var webSocket = new WebSocket("ws://" + location.hostname + ":" + location.port + "/chat");
-webSocket.onmessage = function (msg) { update(msg); };
-//webSocket.onclose = function () { alert("WebSocket connection closed") };
 
-// $("#card1").click( function () {
-//     alert("card1 is played!");
-//     console.log("clicked card1");
-//     sendJson("1");
-// });
-document.getElementById("card1").addEventListener("click", function () {
-    alert("card1 is played!");
-    document.getElementById("card1").remove();
+
+//Establish the WebSocket connection and set up event handlers
+var webSocket = new WebSocket("ws://" + location.hostname + ":" + location.port + "/chat")
+webSocket.onmessage = function (msg) {
+    console.log("Received message update!")
+    update(msg)
+}
+
+//webSocket.onclose = function () { alert("WebSocket connection closed") }
+
+$("#card1").click( function () {
     sendJson("1");
+});
+// document.getElementById("card1").addEventListener("click", function () {
+//     alert("card1 is played!")
+//     document.getElementById("card1").remove()
+//     console.log("removed!")
+//     sendJson("1")
+// })
+
+// document.getElementById("card2").addEventListener("click", function () {
+//     alert("card2 is played!")
+//     // document.getElementById("card2").remove()
+// })
+
+$("#card2").click( function () {
+    sendJson("2");
+});
+
+$("#card3").click( function () {
+    alert("card3 is played!")
+    sendJson("3")
 })
 
-document.getElementById("card2").addEventListener("click", function () {
-    alert("card2 is played!")
-    document.getElementById("card2").remove();
-});
-
-// $("#card2").click( function () {
-//     alert("card2 is played!")
-//     sendJson("2");
-// });
-
-$("#card3").click(function () {
-    sendJson("3");
-});
-
 $("#card11").click(function () {
-    sendJson("11");
-});
+    sendJson("11")
+})
 
 $("#card22").click(function () {
-    sendJson("22");
-});
+    sendJson("22")
+})
 
 $("#card33").click(function () {
-    sendJson("33");
+    sendJson("33")
 })
 
 // //Send message if enter is pressed in the input field
@@ -47,25 +52,29 @@ $("#card33").click(function () {
 
 
 function sendJson(string) {
-    webSocket.send(JSON.stringify({play: string}));
-    var card = "#card" + string;
-    // document.getElementById(card).remove();
+    console.log("card" + string + " is played!")
+    webSocket.send(JSON.stringify({play: string}))
+    console.log("Message sent!")
+    var card = "#card" + string
+    $(card).remove()
 }
 
 function update(message) {
-    $.ajax({
-        url: "/testget/",
-        type: "GET",
-        data: {'input': message},
-    })
-        .done( function(data) {
-            alert("success: " + data);
-            var card = "card" + JSON.parse(data).play;
-            $(card).remove();
-    })
-        .fail( function(data) {
-            console.log("error: " + data)
-        })
+    // $.ajax({
+    //     url: "/testget/",
+    //     type: "GET",
+    //     data: {'input': message}
+    // })
+    //     .done( function(data) {
+    //         console.log("'GET' method success: " + data)
+    //         var card = "#card" + JSON.parse(data).play
+    //         $(card).remove()
+    // })
+    //     .fail( function(data) {
+    //         console.log("error: " + data)
+    //     })
+    var card = "#card" + JSON.parse(message.data).play
+    $(card).remove()
 }
 
 
