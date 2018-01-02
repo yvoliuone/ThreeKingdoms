ce1cf0880eae3d8cdf9e39bab1eaab7b046a1ff6
yvoliuone <yvoliuone@gmail.com>
2018-01-02 17:12:40 +0800

    WebSocket Learning Notes (手牌回合)

--- src/main/java/testChatServer/Chat.java
+++ src/main/java/testChatServer/Chat.java
@@ -1,9 +1,12 @@
 package testChatServer;
 
+import game.ThreeKingdoms;
+import game.players.Player;
 import org.eclipse.jetty.websocket.api.Session;
 import org.json.JSONObject;
 
 
+import java.io.IOException;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -11,40 +14,36 @@ import static j2html.TagCreator.*;
 import static spark.Spark.*;
 
 public class Chat {
-  static Map<Session, String> userUsernameMap = new ConcurrentHashMap<>();
-  static int nextUserNumber = 1;
+  public static int userNumber = 1;
+  public static ConcurrentHashMap<Session, Integer> userMap = new ConcurrentHashMap();
 
 
   public static void main(String[] args) {
+    ThreeKingdoms game = ThreeKingdoms.startNewGame(Player.createTest("player1"), Player.createTest("player2"));
     staticFileLocation("/public");
-//    staticFiles.expireTime(600);
     webSocket("/chat", ChatWebSocketHandler.class);
-    init();
+
+    // launch the browser
+    try {
+      java.awt.Desktop.getDesktop().browse(java.net.URI.create("http://localhost:4567/"));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    get("/testget/", (request, response) ->  request.queryParams("input") );
+
+
   }
 
-
-  private static String createHtmlMessageFromSender(String sender, String message) {
-    return article().with(
-        b(sender + " plays " + message),
-        p(message)
-//        span().withClass("timestamp").withText(new SimpleDateFormat("HH:mm:ss").format(new Date()))
-    ).render();
+  public static void sendMessage(Session userSession, String message) {
+    try {
+      nextSession(userSession).getRemote().sendString(message);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 
-
-  public static void broadcastMessage(String sender, String message) {
-    userUsernameMap.keySet().stream().filter(Session::isOpen).forEach(session -> {
-      try {
-        session.getRemote().sendString(String.valueOf(new JSONObject()
-            .put("userMessage", createHtmlMessageFromSender(sender, message))
-            .put("userlist", userUsernameMap.values())
-        ));
-      } catch (Exception e) {
-        e.printStackTrace();
-      }
-        }
-
-    );
+  public static Session nextSession(Session userSession) {
+    return userMap.keySet().stream().filter(session -> session != userSession).findFirst().get();
   }
-
 }
--- src/main/java/testChatServer/ChatWebSocketHandler.java
+++ src/main/java/testChatServer/ChatWebSocketHandler.java
@@ -2,31 +2,37 @@ package testChatServer;
 
 import org.eclipse.jetty.websocket.api.*;
 import org.eclipse.jetty.websocket.api.annotations.*;
+import org.json.JSONObject;
 
 @WebSocket
 public class ChatWebSocketHandler {
 
-  private String sender, msg;
-
+//  private String sender, msg;
+//
   @OnWebSocketConnect
-  public void onConnect(Session user) throws Exception {
-    String username = "Player" + Chat.nextUserNumber++;
-    Chat.userUsernameMap.put(user, username);
-    Chat.broadcastMessage(sender = "Server", msg = (username + " joined the game"));
+  public void onConnect(Session userSession) throws Exception {
+      int userId = Chat.userNumber++;
+      System.out.println("Welcome user" + userId);
+      Chat.userMap.put(userSession, userId);
+//    String username = "Player" + Chat.nextUserNumber++;
+//    Chat.userUsernameMap.put(user, username);
+//    Chat.broadcastMessage(sender = "Server", msg = (username + " joined the game"));
   }
-
-
+//
+//
   @OnWebSocketClose
-  public void onClose(Session user, int statusCode, String reason) {
-    String username = Chat.userUsernameMap.get(user);
-    Chat.userUsernameMap.remove(user);
-    Chat.broadcastMessage(sender = "Server", msg = (username + " left the game"));
+  public void onClose(Session userSession, int statusCode, String reason) {
+//    String username = Chat.userUsernameMap.get(userSession);
+//    Chat.userUsernameMap.remove(userSession);
+//    Chat.broadcastMessage(sender = "Server", msg = (username + " left the game"));
   }
-
-
+//
+//
   @OnWebSocketMessage
-  public void onMessage(Session user, String message) {
-    Chat.broadcastMessage(sender = Chat.userUsernameMap.get(user), msg = message);
+  public void onMessage(Session userSession, String message) {
+      Chat.sendMessage(userSession, message);
   }
+
+
 
 }
